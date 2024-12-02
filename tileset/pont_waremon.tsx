60c99590a845623093104fd132c3657d56a762a6
fascix <167018661+fascix@users.noreply.github.com>
2024-12-02 17:32:40 +0100

    Delete tileset directory

--- tileset/pont_waremon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileset4g" tilewidth="64" tileheight="64" tilecount="21000" columns="140">
- <image source="pont_waremon.png" width="524" height="248"/>
-</tileset>
