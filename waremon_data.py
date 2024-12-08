baefe4d38f96f7475c8c6f3dc8f415cf700f4c70
Dracoflamme59 <benjamin.huon@live.fr>
2024-12-08 15:08:33 +0100

    Add files via upload

--- waremon_data.py
+++ waremon_data.py
@@ -1,4 +1,88 @@
 TRAINER_DATA = {
+    'blond': {
+                'dialog': { 'default': ['Salut!' , ' Enfin une nouvelle tête!' , ' Ca te dit un combat?'],
+                            'vaincu': ['Tu es plus fort que je le pensais!', 'Bien joué ']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'Boss': {
+                'dialog': {'default': ['Je t attendais! ', ' Battons nous!'],
+                           'vaincu': ['ARGH!', 'Je te retrouv....']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'Daronne': {
+                'dialog':{'default': ['Bonjour, tu veux etre mon ami ?', 'Pardon... Je voulais dire ennemie!'],
+                          'vaincu': ['Bon courage pour la suite!']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'Dresseur': {
+                'dialog': {'default': ['J adore les nouveaux defis!', 'COMBATTONS!!!'],
+                            'vaincu': ['Bien joué...', 'Va voir le boss, tu vas souffrir!']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'fire_boss': {
+                'dialog': {'deafult': ['J adore jouer avec le feu!', 'JOUONS!!'],
+                           'vaincu': ['Bonne chance...']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'grass_boss': {
+                'dialog': {'default': ['Tu oses fouler ma terre ?!', 'Montre moi ce que tu vaut !'],
+                           'vaincu': ['#@*%&']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'hat_girl': {
+                'dialog': {'default':['Salut toi!','J ai un petit jeu...'],
+                           'vaincu':['On peut plus jouer entres amis...']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'purple_girl': {
+                'dialog': {'default': ['Les tenebres vont s abattre sur toi!!'],
+                           'vaincu': ['Les tenebres sont immortels....']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'straw': {
+                'dialog': {'default': ['Salut toi!','Je m ennuis...', 'Ca te dit un combat?'],
+                           'vaincu': ['Bien joué', 'A bientot.']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'water_boss': {
+                'dialog': {'default': ['Je vais te massacrer!!'],
+                           'vaincu': ['ARGH..','Je reviendr....']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'young_girl': {
+                'dialog': {'default': ['Un petit combat ca te dit?'],
+                           'vaincu': ['Tu as triché!', 'Je te battrai!!']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    },
+    'young_guy': {
+                'dialog': {'default': ['Hey!', 'Tu as deja gouté a la defaite?', 'Non? Tu vas voir!!'],
+                           'vaincu': ['Très mauvais goût la defaite...']},
+                'directions': ['down'],
+                'look_around': True,
+                'vaincu': False,
+    }
 
 }
 
